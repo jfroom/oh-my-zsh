3ce2b6a0e6df597ee85a2ef105e6b5c0823e323a
Jeff Froom <jeff@jfroom.com>
2013-02-13 12:14:19 -0600

    no message

--- plugins/node/node.plugin.zsh
+++ plugins/node/node.plugin.zsh
@@ -1,5 +1,7 @@
 # Open the node api for your current version to the optional section.
 # TODO: Make the section part easier to use.
+
+export NODE_PATH="/usr/local/lib/node"
 function node-docs {
 	open "http://nodejs.org/docs/$(node --version)/api/all.html#all_$1"
 }
--- plugins/npm/npm.plugin.zsh
+++ plugins/npm/npm.plugin.zsh
@@ -1,1 +1,2 @@
 eval "$(npm completion 2>/dev/null)"
+set PATH=$PATH:/usr/local/share/npm/bin
