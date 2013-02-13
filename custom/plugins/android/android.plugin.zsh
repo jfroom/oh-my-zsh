37027c73d3237a4e3301c0b05e61df198eacc4be
Jeff Froom <jeff@jfroom.com>
2013-02-13 00:08:10 -0600

    no message

--- custom/plugins/android/android.plugin.zsh
+++ custom/plugins/android/android.plugin.zsh
@@ -1,6 +1,6 @@
 # Add your own custom plugins in the custom/plugins directory. Plugins placed
 # here will override ones with the same name in the main plugins directory.
-
+export PATH=$PATH:/Applications/ANDROID/tools/platform-tools:/Applications/ANDROID/tools/tools
 
 function android_emulate { emulator -avd "$1" -partition-size 1024 &}
 #export -f androidemulate
--- custom/plugins/weinre/weinre.plugin.zsh
+++ custom/plugins/weinre/weinre.plugin.zsh
@@ -1,8 +1,3 @@
-# Add your own custom plugins in the custom/plugins directory. Plugins placed
-# here will override ones with the same name in the main plugins directory.
-
-
 function weinre_host() { 
 	weinre --httpPort 8081 --boundHost local.weinre.com &
 }
-export -f weinrehost
