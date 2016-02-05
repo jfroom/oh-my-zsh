db76e51fdde88a7b4f9c8bc70706946a5e105830
jfroom <jeff@jfroom.com>
2016-02-05 10:17:37 -0800

    add zearn gen cmd

--- custom/plugins/zearn/zearn.plugin.zsh
+++ custom/plugins/zearn/zearn.plugin.zsh
@@ -1,3 +1,6 @@
 export DENT_SKIP_JASMINE=true
 export DENT_MUTE_AUDIO=false
 export DENT_DEBUG_ASSETS=false
+
+#  gen path/to/spec  # use :focus on scenario
+function gen { rake dev_utils:generate_data SPEC="$1"; }
