2190ccf4fb24ebd45a222f6816f5ffb2f196db83
Jeffrey Froom <jeff@cleargenetics.com>
2017-11-27 15:00:13 -0800

    Fix nvm config

--- custom/plugins/nvm/nvm.plugin.zsh
+++ custom/plugins/nvm/nvm.plugin.zsh
@@ -1,15 +1,3 @@
 #!/bin/zsh
-
-#if [[ ! -e ~/.nvm ]]; then
-#	mkdir ~/.nvm
-#	cp ${0:h}/nvm/nvm.sh ~/.nvm
-#fi
-
-#emulate zsh -c '. ~/.nvm/nvm.sh'
-
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-
-export NODE_PATH=${NVM_PATH}:${NVM_PATH}_modules
-#export NODE_PATH=/usr/local/lib/node_modules
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
