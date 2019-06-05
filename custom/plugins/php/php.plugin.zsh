4d29a603897e72cf649714c469ca9ad573958e2f
Jeff Froom <jeff@cleargenetics.com>
2019-06-05 10:13:33 -0700

    update php path

--- custom/plugins/php/php.plugin.zsh
+++ custom/plugins/php/php.plugin.zsh
@@ -1,2 +1,7 @@
 # Homebrew PHP CLI
-export PATH="$(brew --prefix homebrew/php/php56)/bin:$PATH"
+# export PATH="$(brew --prefix homebrew/php/php56)/bin:$PATH"
+
+# export PATH="$(brew --prefix php@5.6)/bin:$PATH"
+export PATH="$(brew --prefix php@7.3)/bin:$PATH"
+
+
